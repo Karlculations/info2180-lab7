1c6b7077a04ed5158e0b490d20abc1eff10c3e03
Karl <karlgoddard0013@gmail.com>
2016-11-16 04:17:51 +0000

    Finished Product

---
+++ world.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#lookup").click(function(){
+        if ($("#all").is(":checked")){
+            $.ajax({
+                type: "GET",
+                url: "world.php",
+                datatype: "html",
+                data: {all: $("#all").val()},
+                success: function(text){
+                    $("#result").html(text);
+                }
+            });
+        }else{
+            $.ajax({
+            type: "GET",
+            url: "world.php",
+            datatype: "html",
+            data: {country: $("#country").val()},
+            success: function(text){
+                alert(text);
+                $("#result").html(text);
+            }
+        });
+        }
+        /*$.ajax({
+            type: "GET",
+            url: "world.php",
+            datatype: "html",
+            data: {country: $("#country").val()},
+            success: function(text){
+                alert(text);
+                $("#result").html(text);
+            }
+        });*/
+    });
+    
+    /*$("#all").click(function(){
+        $.ajax({
+            type: "GET",
+            url: "world.php",
+            datatype: "html",
+            data: {all: $('#all').val()},
+            success: function(text){
+                $("#result").html(text);
+            }
+        });
+    });*/
+});
